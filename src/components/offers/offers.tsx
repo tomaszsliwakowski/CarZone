cd0460f6e29ff69ff21199214508f94a232fbc93
tomaszsliwakowski <tomek08082002@gmail.com>
2024-07-07 14:31:26 +0200

    offer design

--- src/components/offers/offers.tsx
+++ src/components/offers/offers.tsx
@@ -1,5 +1,12 @@
 import { offersList } from "../../utils/offersData";
+import { TbRoad } from "react-icons/tb";
+import { BsFuelPump } from "react-icons/bs";
+import { TbManualGearboxFilled } from "react-icons/tb";
+import { RiCalendarEventLine } from "react-icons/ri";
+import { FiMapPin } from "react-icons/fi";
 import "./offers.scss";
+import { format } from "timeago.js";
+import { Link } from "react-router-dom";
 
 export default function Offers() {
   return (
@@ -10,8 +17,49 @@ export default function Offers() {
             <div className="offer__container-first">
               <img src={offer.img} alt="photo" />
             </div>
-            <div className="offer__container-second">info</div>
-            <div className="offer__container-third">price</div>
+            <div className="offer__container-second">
+              <div className="offer__carInfoContainer">
+                <div className="offer__title">
+                  <Link to={"/"}>{offer.name}</Link>
+                  <p>
+                    {offer.capacity + " cm3"}&nbsp;&bull;&nbsp;
+                    {offer.power + " KM"}
+                    &nbsp;&bull;&nbsp;{offer.shortText}
+                  </p>
+                </div>
+                <div className="offer__carInfo">
+                  <span>
+                    <TbRoad />
+                    {offer.mileage + " km"}
+                  </span>
+                  <span>
+                    <BsFuelPump />
+                    {offer.fuelType}
+                  </span>
+                  <span>
+                    <TbManualGearboxFilled />
+                    {offer.gear}
+                  </span>
+                  <span>
+                    <RiCalendarEventLine />
+                    {offer.productionYear}
+                  </span>
+                </div>
+              </div>
+              <div className="offer__otherInfo">
+                <span>
+                  <FiMapPin />
+                  {offer.location}
+                </span>
+                <p>Published &nbsp;{format(offer.publicAt)}</p>
+              </div>
+            </div>
+            <div className="offer__container-third">
+              <div>
+                <h3>{offer.price}</h3>
+                <p>PLN</p>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
